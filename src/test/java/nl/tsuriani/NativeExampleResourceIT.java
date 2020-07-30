b2619324e833bbf8c52075b3d9e17ca6e5fce46a
quarkusio <no-reply@quarkus.io>
2020-07-30 11:27:52 +0000

    Initial commit

---
+++ src/test/java/nl/tsuriani/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package nl.tsuriani;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
